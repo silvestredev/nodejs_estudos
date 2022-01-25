d3f3ff9e442a41c74b006b3812fbbdaa4d0c00b6
Vitor Silvestre <vitormateus30@yahoo.com.br>
2022-01-25 16:58:57 -0300

    segunda atualização

---
+++ INTRO/primeiroProjeto/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+console.log('O que o Vitor está pensando?');
+
+fs.readFile('pensamentos1.txt', 'utf-8', (err, data) => {
+    if(err){
+        console.log('Este foi o erro:' + ' ' + err);
+    } else {
+        console.log(data);
+    }
+});
